a7e132612959faa4e9b4fb4e92abcb7a9bc3583b
Jesus Arevalo <jarevalo@saludelectronica.com>
2023-03-20 21:40:44 -0500

    Pokedex api

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Se crea un prefijo global ejemplo: http://localhost:3000/api/v2/pokemon
+  app.setGlobalPrefix('api/v2');
+
+  // Pipes globales
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+
+  await app.listen(3000);
+}
+bootstrap();
